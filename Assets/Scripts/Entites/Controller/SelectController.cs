87135e02723d4cd87c4cc44aaf43e74acf591fd1
new-Tower-of-Babel <net6994@gmail.com>
2024-10-18 15:27:15 +0900

    [update]타이틀신 일부수정
    [update]타이틀신 일부수정

--- Assets/Scripts/Entites/Controller/SelectController.cs
+++ Assets/Scripts/Entites/Controller/SelectController.cs
@@ -10,12 +10,14 @@ public class SelectController : MonoBehaviour
     [SerializeField] protected GameObject player2;
     [SerializeField] protected GameObject playerChoose;
     [SerializeField] protected Transform player1Transform;
-    [SerializeField] protected static TextMeshProUGUI playerModeTxt;
+    [SerializeField] protected TextMeshProUGUI playerModeTxt;
+    [SerializeField] private GameObject levelButton;
+    [SerializeField] private GameObject playerMode;
 
     protected string singleMode = "Now SinglePlay";
     protected string MultiMode = "Now MultiPlay";
 
-    protected Vector2 player1SingleModeVector = new Vector2(660,740);
+    protected Vector2 player1SingleModeVector = new Vector2(660, 740);
     protected Vector2 player1MultiModeVector = new Vector2(960, 740);
     public void Start()
     {
@@ -27,30 +29,49 @@ public class SelectController : MonoBehaviour
         {
             player1.SetActive(false);
             playerChoose.SetActive(true);
+            levelButton.SetActive(false);
+            playerMode.SetActive(false);
         }
         else if (playerModeTxt.text==MultiMode)
         {
             player1.SetActive(false);
             player2.SetActive(false);
             playerChoose.SetActive(true);
+            levelButton.SetActive(false);
+            playerMode.SetActive(false);
         }
     }
     public void playerModeChoiceSet()
     {
+        //Debug.Log($"{ playerModeTxt.text} +  +{singleMode}+  + {MultiMode}");
         if (playerModeTxt.text == singleMode)
         {
             playerModeTxt.text=MultiMode;
-            player1Transform.position = player1MultiModeVector;
-        }else if (playerModeTxt.text==MultiMode)
+            player2.SetActive(true);
+            player1Transform.position = player1SingleModeVector;
+        }
+        else if (playerModeTxt.text==MultiMode)
         {
             playerModeTxt.text = singleMode;
-            player1Transform.position = player1SingleModeVector;
+            player2.SetActive(false);
+            player1Transform.position = player1MultiModeVector;
         }
 
     }
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL(https://nbcamp.spartacodingclub.kr/);
+        #else
+            Application.Quit();
+        #endif
+    }
 }
-public class SkinSelectController : SelectController
+public class characterSkinChoose : MonoBehaviour
 {
+    public void 
 
 }
 public class PlayModeSelectController : SelectController
