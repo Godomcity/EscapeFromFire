075ed4fa52b839b184dbea466c8e7cd7c0f66786
Seo-Woongbi <suwoongby@naver.com>
2024-10-18 17:52:23 +0900

    Update MonsterFireBall

--- Assets/Scripts/FireBall/MonsterFireBall.cs
+++ Assets/Scripts/FireBall/MonsterFireBall.cs
@@ -16,7 +16,7 @@ public class MonsterFireBall : Balls
 
     protected override void OnTriggerEffect(Collider2D collision)
     {
-        Vector3 collisionPoint = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        Vector3 collisionPoint = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y-0.5f, gameObject.transform.position.z);
         
         int randomMonster = Random.Range(0, monsterPrefabs.Length);
         Instantiate(monsterPrefabs[randomMonster], collisionPoint,Quaternion.identity);
