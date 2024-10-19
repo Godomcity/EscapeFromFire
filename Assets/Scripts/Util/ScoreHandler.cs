6ea0f71a43e634f1313adee806b121171967fc9d
lostwaltz <lostwaltz89@gmail.com>
2024-10-19 16:35:47 +0900

    fix fireball , movement class

---
+++ Assets/Scripts/Util/ScoreHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    private float score;
+    private TMP_Text scoreText;
+    private float scoreMultiplier = 100f;
+
+    private void Awake()
+    {
+        score = 0;
+        scoreText = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        score += Time.deltaTime * scoreMultiplier;
+        scoreMultiplier += Time.deltaTime;
+
+        scoreText.text = ((int)score).ToString();
+    }
+}
